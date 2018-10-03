d424530dcb3f778c65270199bc673cee924b0e5e
VirajJain <viraj.jain4321@gamil.com>
2018-10-03 14:39:22 -0400

    debugged amount of left

--- student.py
+++ student.py
@@ -104,7 +104,7 @@ class Piggy(pigo.Pigo):
         for x in range(4):
             if not self.is_clear():
                 return False
-            self.encL(7.5)
+            self.encL(7)
         # if we find no problems
         return True
 
