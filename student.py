88246617f1d4d2cab9bd16d2744fc67a2a8a36ec
VirajJain <viraj.jain4321@gamil.com>
2018-10-17 14:37:47 -0400

    changed turns again and speed

--- student.py
+++ student.py
@@ -65,11 +65,11 @@ class Piggy(pigo.Pigo):
                 for x in range(4):
                     self.head_left()
                     self.head_right()
-                self.encL(18)
-                self.set_speed(60, 60)
+                self.encL(20)
+                self.set_speed(70, 70)
                 self.half_forward()
                 self.set_speed(self.LEFT_SPEED, self.RIGHT_SPEED)
-                self.encR(18)
+                self.encR(20)
                 self.encF(18)
             time.sleep(.1)
 
