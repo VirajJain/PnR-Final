9eaa1d8072f3ed7b294d28dd4f1800ce2f77d60f
VirajJain <viraj.jain4321@gamil.com>
2018-10-24 15:14:37 -0400

    fixed Midpoint command

--- student.py
+++ student.py
@@ -69,7 +69,7 @@ class Piggy(pigo.Pigo):
             left_total = 0
             right_total = 0
             # loop from self.MIDPOINT - 60 to self.MIDPOINT
-            for angle in range(self.MIDPOINT, self.MIDPOINT - 60):
+            for angle in range(self.MIDPOINT - 60, self.MIDPOINT):
                 if self.scan[angle]:
                     # add up the numbers to right_total
                     right_total += self.scan[angle]
