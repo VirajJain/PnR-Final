b23d9469135f2046828e38d47244cd82b6ae5c36
VirajJain <viraj.jain4321@gamil.com>
2018-10-26 14:22:58 -0400

    fixed amount of turn again

--- student.py
+++ student.py
@@ -91,7 +91,7 @@ class Piggy(pigo.Pigo):
             # while it's not clear
             while not self.is_clear():
                 # turn
-                self.encL(5)
+                self.encL(2)
             pass
 
 
