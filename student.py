27e325cafa177ae8014f8233e6169f55f4b0197b
VirajJain <viraj.jain4321@gamil.com>
2018-10-26 14:28:30 -0400

    reduced turn amount

--- student.py
+++ student.py
@@ -91,7 +91,7 @@ class Piggy(pigo.Pigo):
             # while it's not clear
             while not self.is_clear():
                 # turn
-                self.encL(2)
+                self.encL(1)
             pass
 
 
