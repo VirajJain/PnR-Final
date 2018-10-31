07e849e3542cb697a784a2e9044e7f64c6addea9
VirajJain <viraj.jain4321@gamil.com>
2018-10-31 14:37:54 -0400

    added right turn again

--- student.py
+++ student.py
@@ -257,7 +257,7 @@ class Piggy(pigo.Pigo):
             else:
                 self.encL(8)
                 if not self.is_clear():
-                    self.encR(16)
+                    self.encR(17)
                 if not self.is_clear():
                     self.encR(8)
 
